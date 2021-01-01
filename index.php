d248892bb0eefd2401bc58a7aae3b343489e09c5
hew_ka_kian <hew_ka_kian@SFT-OIS30957.rp.edu.sg>
2021-01-02 02:35:05 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Story Teller: Our Stories</title>
+        <link href="stylesheets/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+       Hi
+
+    </body>
+</html>
